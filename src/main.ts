d1fe5dbf86208976778789b665707855dac1d1d9
risingcurve <devonebluu@gmail.com>
2025-06-15 23:46:34 +0900

    :sparkles: cors error fix

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ async function bootstrap() {
     forbidNonWhitelisted: true, // DTO에 정의되지 않은 속성이 있으면 에러 발생 (오타 등 방지)
     transform: true,         // 요청 페이로드를 DTO 타입 인스턴스로 자동 변환 (필수)
   }));
+  
+  app.enableCors({
+    origin: 'http://localhost:3001', // React 앱이 실행되는 포트
+    credentials: true,
+  });
 
   await app.listen(3000);
 }
